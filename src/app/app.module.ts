bf53215c62b98f7c976a523956c9fa2829f07cd9
Herold Kristóf <herold.kristof@microsec.hu>
2019-12-19 16:24:48 +0100

    Its a start...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule} from '@angular/material/button'
+import { JokeBoxComponent } from './moraleassistant/joke-box/joke-box.component';
+import { SoundBarComponent } from './moraleassistant/sound-bar/sound-bar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeBoxComponent,
+    SoundBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
